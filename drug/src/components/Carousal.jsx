87e7762d70a13436c53500dbe855e6921275177c
traptijindal <traptijindal5678@gmail.com>
2024-02-03 18:57:04 +0530

    carousel added

---
+++ drug/src/components/Carousal.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import { Navigation, Pagination,Autoplay,Mousewheel} from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import "../CSS/Carousel.css"
+
+const Carousal = () => {
+  return (
+    <>
+      <div className="main-carousel -z-1">
+      <Swiper
+        //   spaceBetween={}
+        slidesPerView={1}
+        // centeredSlides={true}
+        pagination={{
+          clickable:true,
+        }}
+        loop={true}
+        autoplay={{
+          delay: 2500,
+          // disableOnInteraction: false,
+        }}
+  
+        cssMode={true}
+        sticky={false}
+        freeMode={{ enable: true }}
+        mousewheel={{
+          mousewheel: true,
+        }}
+        // navigation={true}
+        breakpoints={{
+          0: {
+            slidesPerView: 1,
+            
+          },
+          640: {
+            slidesPerView: 1,
+          },
+        }}
+        modules={[Autoplay, Pagination, Navigation, Mousewheel]}
+      >
+        <SwiperSlide>
+          <img className=" w-full height" src="\images\c_1.jpg" alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img className=" w-full height" src="\images\c_2.jpg" alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img className=" w-full height" src="\images\c_3.jpg" alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img className=" w-full height" src="\images\c_5.jpg" alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img className=" w-full height" src="\images\c_7.jpg" alt="" />
+        </SwiperSlide>
+      </Swiper>
+      </div>
+    </>
+  );
+}
+
+export default Carousal
