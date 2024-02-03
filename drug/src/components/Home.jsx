5f651573bdbb7e0f88b49f52e8930343fa2f547d
traptijindal <traptijindal5678@gmail.com>
2024-02-03 19:54:20 +0530

    error

--- drug/src/components/Home.jsx
+++ drug/src/components/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import Header from './Header'
+import Carousal from './Carousal'
 
 
 
 function Home() {
   return (
     <div>
-    
+    <Header/>
+    <Carousal/>
+    <Footer/>
     </div>
   )
 }
