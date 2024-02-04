0f6003371c52bd23cda8ef04066554d8d0fc4a0c
traptijindal <traptijindal5678@gmail.com>
2024-02-04 22:39:10 +0530

    bot added

--- drug/src/components/Home.jsx
+++ drug/src/components/Home.jsx
@@ -6,22 +6,23 @@ import Faq from './FAQ'
 import Cards from './Cards'
 import Content from './Content'
 import Acheivements from './Acheivements'
+import { Link } from 'react-router-dom'
 
 
 
 
 function Home() {
   return (
-    <div style={{ backgroundColor: ' #F2FFE9' }}>
+    <div>
+      <Link to="https://mediafiles.botpress.cloud/0f4a7564-720b-475b-a00f-17b5e0676fe5/webchat/bot.html">
 <button class="fixed bottom-7 right-7 p-2 bg-blue-500 hover:bg-blue-900 text-white rounded-full w-16 h-16 text-3xl">
     💬 
 </button>
-
+</Link>
      <Header/>
     <Carousal/>
     <Content/>
-    
-   <Cards/>
+    <Cards/>
    <Acheivements/>
     <Faq/>
     <Footer/>
