a292aab31f306eaae987324a12939430f8952581
traptijindal <traptijindal5678@gmail.com>
2024-02-05 12:42:59 +0530

    councellor

--- drug/src/components/Acheivements.jsx
+++ drug/src/components/Acheivements.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 function Achievements() {
   return (
-    <div className='mx-5 md:mx-20 my-5 md:my-10 p-4 md:p-7 text-white rounded-xl' style={{ backgroundColor: '#228B22' }}>
+    <div className='mx-5 md:mx-20 my-5 md:my-10 p-4 md:p-7 text-black rounded-xl bg-blue-400  shadow-lg shadow-slate-400' >
       <h1 className='text-2xl md:text-3xl mb-4 md:mb-7 font-bold'>Our Achievements</h1>
-      <p className='text-base md:text-lg'>
+      <p className='text-base md:text-lg '>
         50 years of successful co-operation on drug policies and human rights - the Pompidou Group provides tools that enable decision makers to implement effective measures to reduce drug trafficking, better treat substance use disorders, and address the associated negative impacts in society.
       </p>
     </div>
--- drug/src/components/Councellor.jsx
+++ drug/src/components/Councellor.jsx
@@ -1,11 +1,79 @@
 import React from 'react'
+import Header from './Header';
+import Footer from './Footer';
 
 function Councellor() {
-  return (
-    <div>
-     
-    </div>
-  )
-}
+          const data=[
+           {id:1,
+            image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnEhNX3QyKFr-rtiuuQ_z6Oq-wpDtHqQsqueI42dWcP_RpCU6_KZXXbf0gUQQAU9F4sLo&usqp=CAU",
+            name:"PHD",
+            contact:"9219xxxxxx",
+            experience: "I have 3 years of working experi" ,
 
+
+             },
+
+            {id:2,
+            image: "https://www.centerformentalhealth.in/wp-content/uploads/2022/12/1Sneha-Deolankar.jpg",
+            name: "PHD",
+            contact:"9219xxxxxx",
+            experience:"I have 3 years of working experi",
+             },
+            {id:3,
+            image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5nQ8zf1Saz8qGDgFwtC-ugYqOB8l9uQBVJsIZHk-Dm1sOKeDQuGkuUbdyUbMK_UJWCJI&usqp=CAU",
+            name: "PHD",
+            contact:"9219xxxxxx",
+            experience:"I have 3 years of working experi",
+            },
+            {id:4,
+              image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSq-quVe4U24WDI7eBGRYxQRtrjjIcHFHp4fQ&usqp=CAU",
+              name: "PHD",
+              contact:"9219xxxxxx",
+              experience:"I have 3 years of working experi",
+              },
+            {id:5,
+            image: "https://www.theholisticliving.org.in/wp-content/uploads/2021/02/best_psychologist_in_Delhi_Shivani_Misri_Sadhoo.jpg",
+            name: "PHD",
+            contact:"9219xxxxxx",
+            experience:"I have 3 years of working experi",
+                },
+            {id:6,
+            image: "https://coachformind.com/wp-content/uploads/2021/11/IMG_2263-scaled-e1649253447942.jpg.webp",
+             name: "PHD",
+            contact:"9219xxxxxx",
+            experience:"I have 3 years of working experi",
+             },
+            
+            
+    ]
+  return (
+    <>
+    <Header/>
+    <h1 className="text-4xl font-bold text-center mb-8">Our Best Counsellor</h1>
+    <div className="w-full flex flex-wrap justify-center mt-12 gap-8 ">
+    {data.map((counselor) => (
+        <div
+          key={counselor.id}
+          className="overflow-hidden flex flex-col md:flex-row md:max-w-[560px] w-full p-4 border border-solid border-black rounded-xl shadow-lg shadow-slate-400 bg-blue-300"
+        >
+          <div className="mb-4 md:mb-0 md:mr-4">
+            <img
+              className="h-[200px] md:w-[200px] object-cover rounded-full mx-auto md:mx-0"
+              src={counselor.image}
+              alt=""
+            />
+          </div>
+          <div className="p-4">
+            <h3 className="text-xl font-semibold mb-2">{counselor.name}</h3>
+            <p>Lorem ipsum dolor sit amet consectetur.</p>
+            <h3 className="text-xl font-semibold mb-2">{counselor.experience}</h3>
+            <h3 className="text-xl font-semibold mb-2">{counselor.contact}</h3>
+          </div>
+        </div>
+      ))}
+    </div>
+    <Footer/>
+    </>
+  );
+}
 export default Councellor
